c263b5f35e8e6548d4c0b755fbcf4aece9397ee4
vanya <90196679+vnymyz@users.noreply.github.com>
2026-01-30 13:27:00 +0700

    Perubahan pada fibonacchi

--- Pert10 - Recursion & Backtracking/fibonacchi1.cpp
+++ Pert10 - Recursion & Backtracking/fibonacchi1.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int fibonacchi(int n){
-    // f1 = 0
-    // f2 = 1
-    if(n == 0 || n == 1){   // base case
-        cout << n << endl;
-        return n;
-    } else {
-        cout << n << endl;
-        return(fibonacchi(n-1) + fibonacchi(n-2)); // recursive case
-    }
+int fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main(){
-    int n, i, j = 0;
+    int n;
     cout << "Batas deret : ";
     cin >> n;
-    cout << "Deret Fibonacchi : \n";
-    for(i = 1; i <= n; i++){
-        cout << "f " << i << " : ";
-        cout << fibonacchi(j) << endl;
-        j++;
+
+    cout << "Deret Fibonacci:\n";
+    for(int i = 0; i < n; i++){
+        cout << "f " << i << " : " << fibonacci(i) << endl;
     }
+
     return 0;
 }
