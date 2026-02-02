45338c004e1684634055ab4a61b0e513c8c7437a
vanya <90196679+vnymyz@users.noreply.github.com>
2026-02-02 14:35:11 +0700

    Implement binary tree and traversal functions

--- Pert13 - Binary Tree & Traversal/binary.cpp
+++ Pert13 - Binary Tree & Traversal/binary.cpp
@@ -1,41 +1,68 @@
 #include <iostream>
 using namespace std;
 
+// membuat struktur node
 struct Node {
+    // data dan pointer ke anak kiri dan kanan
     char data;
+    // pointer ke anak kiri
     Node* left;
+    // pointer ke anak kanan
     Node* right;
 
+    // konstruktor nyimpen nilai
     Node(char val) {
+        // nyimpen nilai ke data atau node atau ke pohon
         data = val;
+        // inisialisasi anak kiri dan kanan ke nullptr
+        // atau pohon nya masih kosong
         left = right = nullptr;
     }
 };
 
 // membuat function traversal
 void preorder(Node* root) {
+    // kalau misal pohon nya kosong
+    // maka return atau keluar dari function
     if (root == nullptr) return;
+    // proses node root dulu
     cout << root->data << " ";
+    // lalu rekursif ke anak kiri
     preorder(root->left);
+    // lalu rekursif ke anak kanan
     preorder(root->right);
 }
+// PREORDER ITU ROOT - LEFT - RIGHT
 
+// INORDER ITU LEFT - ROOT - RIGHT
 void inorder(Node* root) {
+    // kalau misal pohon nya kosong
+    // maka return atau keluar dari function
     if (root == nullptr) return;
+    // rekursif ke anak kiri dulu
     inorder(root->left);
+    // proses node root
     cout << root->data << " ";
+    // lalu rekursif ke anak kanan
     inorder(root->right);
 }
 
+// POSTORDER ITU LEFT - RIGHT - ROOT
 void postorder(Node* root) {
+    // kalau misal pohon nya kosong
+    // maka return atau keluar dari function
     if (root == nullptr) return;
+    // rekursif ke anak kiri dulu
     postorder(root->left);
+    // lalu rekursif ke anak kanan
     postorder(root->right);
+    // proses node root
     cout << root->data << " ";
 }
 
 // Program utama
 int main() {
+    // membuat pohon biner contoh
     Node* root = new Node('A');
     root->left = new Node('B');
     root->right = new Node('C');
@@ -43,6 +70,8 @@ int main() {
     root->left->right = new Node('E');
     root->right->right = new Node('F');
 
+    // melakukan traversal
+    // preorder, inorder, postorder function
     cout << "Preorder Traversal: ";
     preorder(root); cout << endl;
 
@@ -52,5 +81,6 @@ int main() {
     cout << "Postorder Traversal: ";
     postorder(root); cout << endl;
 
+    // selesai
     return 0;
 }
